8722f1a47ed811fc6334eeb84f927347ad2cc59d
StartMake111 <startmake4@gmail.com>
2020-10-18 11:52:47 +0300

    good

--- Practice/16/python/code.py
+++ Practice/16/python/code.py
@@ -3,7 +3,7 @@ b=0
 for i in a:
     i = list(i)
     if len(i) != 9 :
-        break
+        continue
     else :
         if i[0] == 'a' and i[4] == '5' and i[5] == '5' and i[6] == '6' and i[7] == '6' and i[8] == '1':
             print(''.join(map(str, i)))
