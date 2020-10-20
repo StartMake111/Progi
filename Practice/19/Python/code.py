dff2b14fe57abc3e0ba81b6c1f73521534bdcc68
StartMake111 <startmake4@gmail.com>
2020-10-20 13:49:06 +0300

    commit

--- Practice/19/Python/code.py
+++ Practice/19/Python/code.py
@@ -1,9 +1,14 @@
 import itertools
 x = int(input())
 a = input()
+b = []
 if len(str(a)) < x :
     a = list(a)
     a += a[0]
     a = ''.join(map(str, a))
-result = ''.join(map(str, itertools.permutations(a, x)))
-print(result)
+result = [item for item in itertools.permutations(a,x)]
+for i in result:
+    for j in range(len(i)):
+        b += i[j]
+    print(''.join(map(str,b)), sep=' ', end= ' ')
+    b.clear()
