47df7ee38ee8e9a0abcd0cd41a2126cfb03882a2
StartMake111 <startmake4@gmail.com>
2020-11-20 15:45:48 +0300

    commit

---
+++ Lab/Lab1/Include/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "cpp-httplib/httplib.h"
+#include "nlohman/json.hpp"
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <fstream>
+using json = nlohmann::json;
+using namespace httplib;
+
+int main(){
+  json hour;
+  json time;
+  json cache;
+  json body;
+
+  Client cli("http://api.openweathermap.org");
+
+  auto res = cli.Get("/data/2.5/onecall?lat=44.9698623&lon=34.1329217&exclude=current,minutely,daily,alerts&units=metric&lang=ru&appid=e1f4f9f81642622cc21d70a6baf981e5");
+  if (cache.empty()){
+    body = cache;  
+  }
+  if (res) {
+
+    if (res->status == 200) {
+
+      auto str = res->body;
+      body = json::parse(str);
+      int size = body["hourly"].size();
+      int time = std::time(0);
+      for (int i = 0; i < size-1; ++i)
+      {
+        if (body["hourly"][i]["dt"] >= time){
+          hour = body["hourly"][i];
+          break;
+        } 
+      }     
+
+    }else{
+
+      std::cout << "Status code: " << res->status << std::endl;
+
+    }
+
+  }
+
+  else {
+
+    auto err = res.error();
+
+    std::cout << "Error code: " << err << std::endl;  
+
+  }
+
+}
