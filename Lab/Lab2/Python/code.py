d48582d6ff072bb52888e537517c0c9e97cd135d
StartMake111 <startmake4@gmail.com>
2020-12-11 19:20:28 +0300

    lab2 client

--- Lab/Lab2/Python/code.py
+++ Lab/Lab2/Python/code.py
@@ -1,4 +1,5 @@
 import openpyxl
+import datetime
 import json
 from flask import Flask, request
 app = Flask(__name__)
@@ -7,12 +8,17 @@ x = '1235'
 columnN,columnID,columnTime,columnItem,columnPrice = 0,1,2,3,4
 book = openpyxl.Workbook()
 sheet = book.active
-sheet[1][0].value,sheet[1][1].value,sheet[1][2].value,sheet[1][3].value,sheet[1][4].value = 'N','User ID', 'Datetime','Item','Price'
+sheet.cell(1,1).value = 'N'
+sheet.cell(1,2).value = 'User ID'
+sheet.cell(1,3).value = 'Datetime'
+sheet.cell(1,4).value = 'Item'
+sheet.cell(1,5).value = 'Price'
 book.save('data.xlsx')
 book.close
 nest = lastadd+1
+timereq = datetime.datetime.now().time()
 @app.route('/', methods = ['POST', 'GET'])
-def index(nest,lastadd):
+def index(nest,lastadd,timereq):
     if request.method == 'POST':
         userid = request.json["user_id"]
         cart = request.json["cart"]
@@ -20,6 +26,7 @@ def index(nest,lastadd):
         sheet = book.active
         sheet[nest][columnN].value = lastadd
         sheet[nest][columnID].value = userid
+        sheet[nest][columnTime].value = timereq
         for item in cart:
             lastadd+=1
             itemname = item['item']
@@ -28,6 +35,7 @@ def index(nest,lastadd):
             sheet[lastadd][columnPrice].value = itemprice
         book.save('data.xlsx')
         book.close
+        return 'POST'
     if request.method == 'GET':
         return "GET"
 if __name__ == "__main__":
