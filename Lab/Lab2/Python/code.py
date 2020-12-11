5a72802f6584618c0ed5b01d6788ef9f1882da4c
StartMake111 <startmake4@gmail.com>
2020-12-11 14:59:04 +0300

    add python code

--- Lab/Lab2/Python/code.py
+++ Lab/Lab2/Python/code.py
@@ -2,19 +2,32 @@ import openpyxl
 import json
 from flask import Flask, request
 app = Flask(__name__)
-lastadd = 0
+lastadd = 1
 x = '1235'
+columnN,columnID,columnTime,columnItem,columnPrice = 0,1,2,3,4
+book = openpyxl.Workbook()
+sheet = book.active
+sheet[1][0].value,sheet[1][1].value,sheet[1][2].value,sheet[1][3].value,sheet[1][4].value = 'N','User ID', 'Datetime','Item','Price'
+book.save('data.xlsx')
+book.close
+nest = lastadd+1
 @app.route('/', methods = ['POST', 'GET'])
-def index():
+def index(nest,lastadd):
     if request.method == 'POST':
         userid = request.json["user_id"]
         cart = request.json["cart"]
+        book = openpyxl.Workbook()
+        sheet = book.active
+        sheet[nest][columnN].value = lastadd
+        sheet[nest][columnID].value = userid
         for item in cart:
+            lastadd+=1
             itemname = item['item']
             itemprice = item['price']
-            
-
-        json.dump(jsontempl, jsonfile, 'w')            
+            sheet[lastadd][columnItem].value = itemname
+            sheet[lastadd][columnPrice].value = itemprice
+        book.save('data.xlsx')
+        book.close
     if request.method == 'GET':
         return "GET"
 if __name__ == "__main__":
