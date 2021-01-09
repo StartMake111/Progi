7f2b7304ff102de030fd9778941959ee2b518a19
StartMake111 <startmake4@gmail.com>
2021-01-09 21:59:39 +0300

    chenge 32

--- Practice/32/C++/code.cpp
+++ Practice/32/C++/code.cpp
@@ -20,14 +20,18 @@ struct Passenger
     string Embarked;
 };
 
-std::istream& operator>> (std::istream& in, std::vector<Passenger>& passengers){
+std::istream &operator>>(std::istream &in, std::vector<Passenger> &passengers)
+{
     csv::CSVReader reader("train.csv");
     float Age;
-    for (auto& el : reader){
-        if (el["Age"].get() == ""){
+    for (auto &el : reader)
+    {
+        if (el["Age"].get() == "")
+        {
             Age = -1;
         }
-        else {
+        else
+        {
             Age = el["Age"].get<float>();
         }
         Passenger passenger{
@@ -47,41 +51,50 @@ std::istream& operator>> (std::istream& in, std::vector<Passenger>& passengers){
     }
     return in;
 }
-std::ostream& operator<< (std::ostream& out, std::vector<Passenger>& passengers){
-    ifstream file;
-    auto lenname = 0;
-    auto lenticket = 0;
-    auto lenFare = 0;
-    auto lencabin = 0;
-    file.open("output.txt");
-    for (auto& pep : passengers){
-        if (pep.Name.length()> lenname) {
-            lenname = pep.Name.length();}
-        if (pep.Ticket.length()> lenticket) {
-            lenticket = pep.Ticket.length();}
-        if (sizeof(pep.Fare)> lenFare) {
-            lenFare = sizeof(pep.Fare);}    
-        if (pep.Cabin.length()> lencabin) {
-            lencabin = pep.Cabin.length();}
-    }
-    for (auto& pep : passengers){
-        out << pep.survive << pep.Pclass << pep.Name << string(lenname - pep.Name.length(), ' ')
-        << pep.Sex << pep.Age << pep.SibSp << pep.Parch << pep.Ticket << string(int(lenticket - pep.Ticket.length()), ' ')
-        << pep.Fare << string(lenFare - sizeof(pep.Fare), ' ')
-        << pep.Cabin << string(lencabin - pep.Cabin.length(), ' ')
-        << pep.Embarked << "\n";
-    }
-    return out;
-}
-int main(){
-        std::vector<Passenger> passengers;
-        std::ifstream data;
-        data.open("train.csv");
-        if (data.is_open()) data >> passengers;
-        data.close();
-        ofstream data1;
-        data1.open("output.txt");
-        if (data.is_open()) data1 << passengers;
-        data1.close();
+std::ostream &operator<<(std::ostream &o, std::vector<Passenger> &passengers)
+{
+    auto writer = csv::make_csv_writer(o);
 
+    writer << std::vector<std::string>{
+        "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"};
+
+    for (auto &pass : passengers)
+    {
+        writer << std::make_tuple(
+            pass.survive,
+            pass.Pclass,
+            pass.Name,
+            pass.Sex,
+            pass.Age,
+            pass.SibSp,
+            pass.Parch,
+            pass.Ticket,
+            pass.Fare,
+            pass.Cabin,
+            pass.Embarked);
     }
+
+    return o;
+}
+int main()
+{
+    std::vector<Passenger> passengers;
+
+    std::ifstream data("train.csv");
+    data >> passengers;
+
+    sort(passengers.begin(), passengers.end(), [&](Passenger a, Passenger b) { return a.Age < b.Age; });
+
+    std::vector<Passenger> onlyFemale;
+    for (auto &pass : passengers)
+    {
+        if (pass.Sex == "female" && pass.Pclass == 1)
+        {
+            onlyFemale.push_back(pass);
+        }
+    }
+    std::ofstream output("output.csv");
+    output << onlyFemale;
+
+    return 0;
+}
