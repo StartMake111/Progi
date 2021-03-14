bf58ffc58d03f38a5b8d348879749e35cac80844
StartMake111 <startmake4@gmail.com>
2021-03-14 15:21:47 +0300

    33

--- Practice/33/Python/code.py
+++ Practice/33/Python/code.py
@@ -1,6 +1,21 @@
 size = int(input("Размер массива "))
 first = int(input("Первый элемент"))
 toplus = int(input("Шаг прогрессии"))
+import builtins
+
+
+def print(mass):
+    if len(mass) < 0:
+        builtins.print("There is no mass")
+        return mass
+    if len(mass) == 0:
+        builtins.print("[]")
+        return mass
+    builtins.print("[", mass[0], sep="", end="")
+    for i in range(1, len(mass)):
+        builtins.print(", ", mass[i], sep="", end="")
+    builtins.print("]\n")
+    return mass
 
 
 def create(size, first=0, toplus=0):
